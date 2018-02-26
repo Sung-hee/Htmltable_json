61b7c582ae188bc3b5b862c049f90093aca9404a
Sung-hee <klpoik@naver.com>
2018-02-26 17:34:30 +0900

    image update, black line remove, real data test

--- jsonTable.js
+++ jsonTable.js
@@ -16,7 +16,8 @@ var power = getParams('power');
 var supply = getParams('supply');
 var perfomance = getParams('perfomance');
 var value = getParams('value');
-var url = 'https://charttest-sungheeek.c9users.io/stock_table.json';
+var url = 'http://61.72.187.6/phps/stock_table?gold=1&power=2&supply=3&performance=4&value=5';
+var height = $(window).height()*0.888;
 
 $(document).ready(function(){
   $.getJSON(url + "?gold=" + gold + "&power=" + power + "&supply=" + supply + "&perfomance=" + perfomance + "&value=" + value, function(data){
@@ -27,50 +28,50 @@ $(document).ready(function(){
     $.each(data, function(key, value){
       json_data += '<tr>';
       json_data += '<td>'+value.companycode+'</td>';
-      if(value.goldagg == "매우우수" || value.goldagg == "우수"){
+      if(value.goldagg == "1 등급" || value.goldagg == "2 등급"){
         json_data += '<td style="color:red">'+value.goldagg+'</td>';
       }
-      else if(value.goldagg == "평범"){
+      else if(value.goldagg == "3 등급"){
         json_data += '<td>'+value.goldagg+'</td>';
       }
       else {
         json_data += '<td style="color:blue">'+value.goldagg+'</td>';
       }
 
-      if(value.powerrating == "매우우수" || value.powerrating == "우수"){
+      if(value.powerrating == "1 등급" || value.powerrating == "2 등급"){
         json_data += '<td style="color:red">'+value.powerrating+'</td>';
       }
-      else if(value.powerrating == "평범"){
+      else if(value.powerrating == "3등급"){
         json_data += '<td>'+value.powerrating+'</td>';
       }
       else {
         json_data += '<td style="color:blue">'+value.powerrating+'</td>';
       }
 
-      if(value.demandrating == "매우우수" || value.demandrating == "우수"){
+      if(value.demandrating == "1 등급" || value.demandrating == "2 등급"){
         json_data += '<td style="color:red">'+value.demandrating+'</td>';
       }
-      else if(value.demandrating == "평범"){
+      else if(value.demandrating == "3 등급"){
         json_data += '<td>'+value.demandrating+'</td>';
       }
       else {
         json_data += '<td style="color:blue">'+value.demandrating+'</td>';
       }
 
-      if(value.performancegrade == "매우우수" || value.performancegrade == "우수"){
+      if(value.performancegrade == "1 등급" || value.performancegrade == "2 등급"){
         json_data += '<td style="color:red">'+value.performancegrade+'</td>';
       }
-      else if(value.performancegrade == "평범"){
+      else if(value.performancegrade == "3 등급"){
         json_data += '<td>'+value.performancegrade+'</td>';
       }
       else {
         json_data += '<td style="color:blue">'+value.performancegrade+'</td>';
       }
 
-      if(value.currentvalue == "매우우수" || value.currentvalue == "우수"){
+      if(value.currentvalue == "1 등급" || value.currentvalue == "2 등급"){
         json_data += '<td style="color:red">'+value.performancegrade+'</td>';
       }
-      else if(value.currentvalue == "평범"){
+      else if(value.currentvalue == "3 등급"){
         json_data += '<td>'+value.currentvalue+'</td>';
       }
       else {
@@ -82,13 +83,14 @@ $(document).ready(function(){
     $('#stock_table').append(json_data);
     $('#stock_table').DataTable( {
       "paging": false,
-      "scrollY": 700,
+      "scrollY": height,
       "scrollX": true,
       "scrollCollapse": true,
       "columnDefs": [
           { width: '15%', targets: 0 }
       ],
-      "fixedColumns": true,
+      // "ordering": false,
+      // "fixedColumns": true,
       "bInfo" : false,
        // "sinfo": false,
        "bFilter": false
